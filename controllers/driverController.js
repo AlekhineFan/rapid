44dc3086d5a29298bdb2f0911bc3fdf7d70f1f53
Mark Szavin <szavinm@yahoo.com>
2020-11-01 20:29:51 +0100

    add search in area

--- controllers/driverController.js
+++ controllers/driverController.js
@@ -1,6 +1,5 @@
 const { request } = require("express");
 const Driver = require("../models/driver");
-const driver = require("../models/driver");
 
 module.exports = {
   greeting(req, res) {
@@ -28,6 +27,26 @@ module.exports = {
     const driverId = req.params.id;
     Driver.findByIdAndRemove({ _id: driverId })
       .then((driver) => res.status(204).res.send(driver))
+      .catch(next);
+  },
+
+  index(req, res, next) {
+    const { lng, lat } = req.query;
+
+    Driver.aggregate([
+      {
+        $geoNear: {
+          near: {
+            type: "Point",
+            coordinates: [parseFloat(lng), parseFloat(lat)],
+          },
+          maxDistance: 200000,
+          spherical: true,
+          distanceField: "dist.calculated",
+        },
+      },
+    ])
+      .then((drivers) => res.send(drivers))
       .catch(next);
   },
 };
