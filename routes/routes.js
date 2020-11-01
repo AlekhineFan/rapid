11e6672ff2e21897959347626720792490e164f5
Mark Szavin <szavinm@yahoo.com>
2020-11-01 20:30:12 +0100

    uncomment / route

--- routes/routes.js
+++ routes/routes.js
@@ -2,8 +2,8 @@ const driverController = require("../controllers/driverController");
 
 module.exports = (app) => {
   app.get("/api", driverController.greeting);
-
   app.post("/api/drivers", driverController.create);
   app.put("/api/drivers/:id", driverController.edit);
   app.delete("/api/drivers/:id", driverController.remove);
+  app.get("/api/drivers", driverController.index);
 };
