61c7cb7c6959fe9221cfb24ed9fd84d84d555c82
Mark Szavin <szavinm@yahoo.com>
2020-11-01 11:45:26 +0100

    add error handling for create drivers, define dev and test db

--- app.js
+++ app.js
@@ -3,11 +3,41 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const routes = require("./routes/routes");
 const app = express();
+const { before, beforeEach } = require("mocha");
 
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://localhost/rapid", { useNewUrlParser: true });
+if (process.env.NODE_ENV !== "test") {
+  mongoose.connect("mongodb://localhost/rapid", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+
+  if (process.env.NODE_ENV === "test") {
+    before((done) => {
+      mongoose.connect("mongodb://localhost/rapid_test");
+      mongoose.connnection
+        .once("open", () => done())
+        .on("error", (err) => {
+          console.warn("Warning", err);
+        });
+    });
+
+    beforeEach((done) => {
+      const { drivers } = mongoose.connection.collections;
+      drivers
+        .drop()
+        .then(() => done())
+        .catch(() => done());
+    });
+  }
+}
 
 app.use(bodyParser.json());
 routes(app);
+
+app.use((err, req, res, next) => {
+  //console.log(err);
+  res.status(422).send({ error: err.message });
+});
 
 module.exports = app;
