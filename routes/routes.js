a73b732d58f49f987678e5c4fcaab0adc802333f
Mark Szavin <szavinm@yahoo.com>
2020-11-01 19:17:23 +0100

    add routes put, delete

--- routes/routes.js
+++ routes/routes.js
@@ -4,4 +4,6 @@ module.exports = (app) => {
   app.get("/api", driverController.greeting);
 
   app.post("/api/drivers", driverController.create);
+  app.put("/api/drivers/:id", driverController.edit);
+  app.delete("/api/drivers/:id", driverController.remove);
 };
